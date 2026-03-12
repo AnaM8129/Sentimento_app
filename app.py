5b271ed5e1d466b84cb905581508bbe1b6f41d31
Ana María <120389092+AnaM8129@users.noreply.github.com>
2026-03-12 13:18:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 from PIL import Image
 from googletrans import Translator
 from streamlit_lottie import st_lottie
+import json
 
 st.title('Análisis de Sentimiento')
 image = Image.open('emoticones.jpg')
